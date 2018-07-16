cc2df01aa9aae69df2e1fd5c3219697f756189ce
minuk8932 <minuk8932@naver.com>
2018-07-16 21:43:56 +0900

    add Sorting words in file

---
+++ ex_1001/tuple_6.py
@@ -0,0 +1,19 @@
+fhand = open('romeo.txt')
+
+counts={}
+for line in fhand:
+    words = line.split()
+
+    for w in words:
+        counts[w] = counts.get(w, 0) + 1
+
+lst = []
+for k, v in counts.items():
+    newtup = (v, k)
+    lst.append(newtup)
+
+lst = sorted(lst, reverse= True)    # 내림차순 정렬
+
+
+for v, k in lst[:10]:
+    print()
