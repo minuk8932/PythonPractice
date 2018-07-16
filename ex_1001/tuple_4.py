0fc5a3c2e1a5db47fdd7f8fe01b4c6e3e6b0e7cd
minuk8932 <minuk8932@naver.com>
2018-07-16 21:08:51 +0900

    add Compare values by Tuple

---
+++ ex_1001/tuple_4.py
@@ -0,0 +1,17 @@
+# true
+t1 = (0, 1, 2)
+t2 = (5, 1, 2)
+
+# true
+# t1 = (0, 1, 2000000)
+# t2 = (0, 3, 4)
+
+# true
+# t1 = ('Jones', 'Sally' )
+# t2 = ('Jones', 'Sam')
+
+# true
+#t1 = ( 'Jones', 'Sally')
+# t2 = ('Adams', 'Sam')
+
+print(t1 < t2)
